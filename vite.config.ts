3e94d4fd9e9d9f84ec03771fff40a88bf3c6e248
m.swiatnicki <m.swiatnicki@7technology.pl>
2025-01-10 22:54:49 +0100

    Configure Vite to load environment variables and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  const envVariables = Object.fromEntries(
+    Object.entries(env).map(([key, value]) => [
+      `import.meta.env.${key}`,
+      JSON.stringify(value)
+    ])
+  )
+  return {
+    define: envVariables,
+    plugins: [react()],
+  }
 })
